42798f1574e020f7d609fa10f41695296ba37e12
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-10-31 13:03:14 +0530

    navbar structure and css added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,8 +2,8 @@
 import React, { useState } from 'react'
 import "./Navbar.scss"
 import { images } from '../../constants'
-import {HiMenuAlt4, HiX} from 'react-icons/hi';
-import {motion} from 'framer-motion';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 
 
@@ -11,42 +11,41 @@ const Navbar = () => {
 
     const [toggle, setToggle] = useState(false);
     return (
-        <nav className='app__navbar'>
-            <div className='ap__navbar-logo'>
-                <img src={images.logo} alt='logo' />
+        <nav className="app__navbar">
+            <div className="app__navbar-logo">
+                <img src={images.logo} alt="logo" />
             </div>
-
-            <ul className='app__navbar-links'>
-                {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
-                    <li className='app__flex p-text' key={`link-${item}`}>
-                    <div/>
-                    <a href={`#${item}`}>{item}</a>
-                     </li>
-                )) }
+            <ul className="app__navbar-links">
+                {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                    <li className="app__flex p-text" key={`link-${item}`}>
+                        <div />
+                        <a href={`#${item}`}>{item}</a>
+                    </li>
+                ))}
             </ul>
 
-            <div className='app__navbar-menu'>
-               <HiMenuAlt4 onClick={() =>setToggle(true)}/>
-               {
-                toggle && (
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
                     <motion.div
-                    whileInView={{x:[300, 0]}}
-                    transition={{duration: 0.85, ease:'easeOut'}}
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
                     >
-                     <HiX onClick={() => setToggle(false)} />
-                     <ul>
-                     {['home', 'about', 'contact', 'work', 'skills'].map((item) => (
-                    <li key={item}>
-                    <div/>
-                    <a href={`#${item}`}>{item}</a>
-                     </li>
-                )) }
-                </ul>
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+                                <li key={item}>
+                                    <a href={`#${item}`} onClick={() => setToggle(false)}>
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                     </motion.div>
-                )
-               }
+                )}
             </div>
-        </nav> 
+        </nav>
     )
 }
 
