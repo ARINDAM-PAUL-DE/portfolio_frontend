10ddfdc23da69b59c3fe7e37066e8b5b6c55f276
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-11-04 13:41:44 +0530

    small changes added

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -6,13 +6,13 @@ const SocialMedia = () => {
   return (
     <div className='app__social'>
     <div>
-        <SocialIcon url="https://www.linkedin.com/in/arindam-paul-9bb196203/"/>
+        <SocialIcon url="https://www.linkedin.com/in/arindam-paul-9bb196203/" target='_blank' rel='noopener noreferrer'/>
     </div>
     <div>
-    <SocialIcon url="https://twitter.com/Arindam04806544"/>
+    <SocialIcon url="https://twitter.com/Arindam04806544" target='_blank' rel='noopener noreferrer'/>
     </div>
     <div>
-    <SocialIcon url="https://github.com/ARINDAM-PAUL-DE"/>
+    <SocialIcon url="https://github.com/ARINDAM-PAUL-DE" target='_blank' rel='noopener noreferrer'/>
     </div>
     </div>
   )
